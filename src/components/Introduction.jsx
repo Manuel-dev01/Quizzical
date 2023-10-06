6fbfb715d508376456374ad6e4abd79610c7578b
olamiye <olamiyeoecpe2022@futa.edu.ng>
2023-10-06 12:07:39 +0100

    Setup Introduction Section

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Introduction() {
+    return (
+        <div className="intro">
+            <h1>Quizzical</h1>
+            <div className="description">
+                <p>
+                    Quizzical is a fun and engaging quiz application that offers a wide range of quizzes on various topics 
+                    and subjects. Whether you're a trivia enthusiast, a knowledge seeker, or just looking for a way to 
+                    challenge yourself and have some fun, Quizzical has something for everyone.
+                </p>
+            </div>
+            <button>Start Quiz</button>
+        </div>
+    )
+}
