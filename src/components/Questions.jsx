c9811d7ce3e9d7fea25e559a0e323ccb3eb4852c
olamiye <olamiyeoecpe2022@futa.edu.ng>
2023-10-28 22:43:22 +0100

    updated gitignore files

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -12,64 +12,64 @@ export default function Questions(props) {
             async function getQuestion() {
                 const res = await fetch(questionUrl)
                 const data = await  res.json()
-                setQuestion(data.results)
+
+                const formattedQuestions = data.results.map((ques) => {
+                    return {...ques, answers: randomize([...ques.incorrect_answers, ques.correct_answer])}
+                })
+                setQuestion(formattedQuestions)
+
+
             }
         
             getQuestion()
     }, [ questionState ])
 
-    question.forEach((ques) => {
-        ques.incorrect_answers.push(ques.correct_answer)
-        // console.log(ques.incorrect_answers)
-        randomize(ques.incorrect_answers)
-    })
         
     function randomize(values) {
-        let index = values.length,
-        randomIndex
+        let currentIndex = values.length, randomIndex
+
+        while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
     
-        // While there remain elements to shuffle.
-        while (index != 0) {
-        // Pick a remaining element.
-        randomIndex = Math.floor(Math.random() * index)
-        index--
-    
-        // And swap it with the current element.
-        [values[index], values[randomIndex]] = [values[randomIndex], values[index]];
+
+        [values[currentIndex], values[randomIndex]] = [values[randomIndex], values[currentIndex]];
         }
     
         return values;
     }
 
-    // function selectAnswer(index) {
-    //     setQuestion((prevQues) => {
-    //         prevQues.map((ques, i) => {
-    //             return {...ques, selectedQuestion: i === index ? 1 : 0}
-    //         })
-    //     }) 
-    //     // {...ques, selectedQuestion: i === index ? 1 : 0}  
-    // }
+    function selectAnswer(questionIndex, answerIndex) {
+        console.log('User Selected:', question[questionIndex].answers[answerIndex])
 
-    function selectAnswer() {
-        console.log('chose this ansswer')
+        setQuestion(ques => {
+            ques.map((eachQues) => {
+                return {...eachQues, answers: }
+            })
+        })
+
+        // function answers(ques){
+        //     ques.answers.map((ques) => {
+        //         return {answer: ques, selected: true}
+        //     })
+        // }
     }
+
+    console.log(question)
 
     const eachQuestion = question.map((ques, index) => {
         return (
-        <div className='eachQuestion'>
-            <h3 key={index}>{decode(ques.question)}</h3>
+        <div className='eachQuestion' key={index}>
+            <h3>{decode(ques.question)}</h3>
             <div>
-                {ques.incorrect_answers.map((ans) => {
-                    return <button onClick={selectAnswer}> {decode(ans)} </button>
+                {ques.incorrect_answers.map((ans, ansIdx) => {
+                    return <button key={ansIdx} onClick={() => selectAnswer(index, ansIdx)} className={ans.selected ? 'selected' : ''}> {decode(ans)} </button>
                 })}
             </div>
         </div>
         )
     })
 
-    // 
-
-    console.log(question)
   
     return (
         <div>
